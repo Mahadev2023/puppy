3ea0e2c267c5d4a1cf922439d9d66ecfaf530d2c
Mahadev <godbolemahadevkanaka@gmail.com>
2023-05-06 23:51:31 +0530

    BigBarks added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Mahadev2023/puppy
 
 go 1.20
+
+require github.com/Mahadev2023/dog v0.0.0-20230506180247-94347c087743 // indirect
--- puppy.go
+++ puppy.go
@@ -1,5 +1,9 @@
 package puppy
 
+import (
+	"github.com/Mahadev2023/dog"
+)
+
 func Bark() string {
 	return "Woof"
 }
@@ -8,4 +12,23 @@ func Barks() string {
 	return `Woof Woof ...`
 }
 
+func BigBarks() string {
+	return dog.WhenGrounUp(Bark())
+}
+
 // https://github.com/Mahadev2023/puppy
+//eval `ssh-agent -s`
+
+//eval "$(ssh-agent -s)"
+
+//to set github
+/*
+ssh-keygen -t ed25519 -C "godbolemahadevkanaka@gmail.com"
+Generating public/private ed25519 key pair.
+Enter file in which to save the key (/c/Users/godbo/.ssh/id_ed25519):
+*/
+
+//to add the file in git do this always
+//eval "$(ssh-agent -s)"
+//ssh-add ~/.ssh/id_ed25519
+//
